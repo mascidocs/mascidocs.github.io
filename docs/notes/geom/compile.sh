ebec6fb100a789c1fe88088e9fbe8bc20d8eef77
noname <melendresam123@protonmail.com>
2021-07-18 10:08:00 +0800

    Added more postulates and theorems to "geom.html"

--- docs/notes/geom/compile.sh
+++ docs/notes/geom/compile.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 mkdir pics &
-rm -rf *.png &&
+cd pics && rm -rf *.png && cd .. &&
 groff -e -ms -T html "geom.ms" > geom.html &&
 sed -i "s/\"grohtml/\"pics\/grohtml/g" geom.html && mv *.png pics
-
